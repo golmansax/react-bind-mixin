1f37b838230d2aa06cc2f90927fd48f2aa7635f1
Holman Gao <holman@golmansax.com>
2015-03-10 21:18:00 -0700

    Updating test

--- test/bind_mixin_test.jsx
+++ test/bind_mixin_test.jsx
@@ -6,20 +6,28 @@ var expect = chai.expect;
 var BindMixin = require('../lib/bind_mixin');
 var sinon = require('sinon');
 chai.use(require('sinon-chai'));
+var TestUtils = require('react').addons.TestUtils;
+var Component = require('./component_mock');
+var Store = require('./store_mock');
 
 describe('bind_mixin', function () {
-  var Component;
-  var Store;
+  var component;
 
   beforeEach(function () {
+    component = TestUtils.renderIntoDocument(<Component />);
   });
 
   it('sets initial state', function () {
+    expect(component.getDOMNode().text).to.equal('initial');
   });
 
   it('updates state when store changes', function () {
+    Store.setValue('changed');
+    expect(component.getDOMNode().text).to.equal('changed');
   });
 
   it('removes listener when component is unmounted', function () {
+    // Unmount component
+    // Expect that Store is not listening to anything
   });
 });
